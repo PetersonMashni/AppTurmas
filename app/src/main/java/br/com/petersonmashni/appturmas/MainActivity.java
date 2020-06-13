030e22dfdb3b822d0156ef45b7dc5317f692ba0d
Peterson Mashni <petersonmashni@gmail.com>
2020-06-13 17:50:29 -0300

    Menu principal

--- app/src/main/java/br/com/petersonmashni/appturmas/MainActivity.java
+++ app/src/main/java/br/com/petersonmashni/appturmas/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.petersonmashni.appturmas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_main_alunos : alunos();
+            case R.id.menu_main_turmas : turmas();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void turmas() {
+    }
+
+    private void alunos() {
+    }
+
 }
