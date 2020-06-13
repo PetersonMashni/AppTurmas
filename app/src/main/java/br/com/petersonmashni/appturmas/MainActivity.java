483656072c7bccb549cc66e0e55069e276fe07a0
Peterson Mashni <petersonmashni@gmail.com>
2020-06-13 15:37:00 -0300

    Carga Inicial

---
+++ app/src/main/java/br/com/petersonmashni/appturmas/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.petersonmashni.appturmas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
